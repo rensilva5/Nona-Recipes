30615a43295441edfc98706b900b6897a8316835
rensilva5 <renzoasilva@gmail.com>
2023-05-04 23:06:51 -0400

    Working on Category component and styling it

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -11,25 +11,63 @@ import { NavLink } from "react-router-dom";
 function Category() {
   return (
     <List>
-      <NavLink to={"/cuisine/Italian"}>
+      <StyledLink to={"/cuisine/Italian"}>
         <GiFullPizza />
         <h4>Italian</h4>
-      </NavLink>
-      <NavLink to={"/cuisine/American"}>
+      </StyledLink>
+      <StyledLink to={"/cuisine/American"}>
         <GiHamburger />
         <h4>American</h4>
-      </NavLink>
-      <NavLink to={"/cuisine/Thai"}>
+      </StyledLink>
+      <StyledLink to={"/cuisine/Thai"}>
         <GiNoodles />
         <h4>Thai</h4>
-      </NavLink>
-      <NavLink to={"/cuisine/Japanese"}>
+      </StyledLink>
+      <StyledLink to={"/cuisine/Japanese"}>
         <GiChopsticks />
         <h4>Japanese</h4>
-      </NavLink>
+      </StyledLink>
     </List>
   );
 }
-const List = styled.div;
+const List = styled.div`
+display: flex;
+justify-content: center
+margin: 2rem 0rem
+`;
+const StyledLink = styled(NavLink)`
+display: flex;
+flex-direction: column;
+justify-content: center
+align-items: center
+border-radius: 50%;
+margin-right: 2rem;
+text-decoration: none;
+background: linear-gradient(35deg, #494949, #313132);
+width: 6rem;
+height: 6rem;
+cursor: pointer;
+transform: scale(0.8rem);
+
+h4 {
+    color: white;
+    font-size: 0.8rem;
+}
+svg {
+    color: white;
+    font-size: 1.5rem
+}
+&.active {
+    background: linear-gradient(to right, #f27121, #e94057);
+
+    svg {
+        color: white;
+    }
+    h4 {
+        color: white;
+    }
+}
+
+`;
 
 export default Category;
