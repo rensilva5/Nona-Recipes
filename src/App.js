63cd2ec46e417f5c8b1ad0e51c700d2ddded010a
rensilva5 <renzoasilva@gmail.com>
2023-05-06 10:45:26 -0400

    Search component, functioanality, and style created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Pages />
         <Category />
       </BrowserRouter>
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import Home from "./Home";
 import { Route, Routes } from "react-router-dom";
 import Cuisine from "./Cuisine";
+import Searched from "./Searched";
 
 function Pages() {
   return (
     // <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cuisine" element={<Cuisine />} />
-        <Route />
-        <Route />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cuisine" element={<Cuisine />} />
+      <Route path="/searched/:search" element={<Searched />} />
+      <Route />
+    </Routes>
     // </BrowserRouter>
   );
 }
